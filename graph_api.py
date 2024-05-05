843127a8f35af2ac00d5a5acd7950d3d6c516038
doshisahil <sahil000005@gmail.com>
2024-05-05 15:59:49 +0530

    detect files on onedrive and delete it locally
    first draft of code

---
+++ graph_api.py
@@ -0,0 +1,60 @@
+from azure.identity import InteractiveBrowserCredential
+from msgraph import GraphServiceClient
+import os
+from pathlib import Path
+import hashlib
+
+
+class GraphAPI:
+    def __init__(self):
+        credential = InteractiveBrowserCredential(
+            client_id=os.getenv('client_id'),
+            tenant_id=os.getenv('tenant_id'),
+        )
+        scopes = ["User.Read", "Files.Read", "Files.Read.All"]
+        self.client = GraphServiceClient(credentials=credential, scopes=scopes, )
+
+    @staticmethod
+    def sha256sum(filename: str):
+        # BUF_SIZE is totally arbitrary, change for your app!
+        BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+
+        sha2 = hashlib.sha256()
+
+        with open(filename, 'rb') as f:
+            while True:
+                data = f.read(BUF_SIZE)
+                if not data:
+                    break
+                sha2.update(data)
+        return sha2.hexdigest()
+
+    @staticmethod
+    def sha1sum(filename: str):
+        # BUF_SIZE is totally arbitrary, change for your app!
+        BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+
+        sha1 = hashlib.sha1()
+
+        with open(filename, 'rb') as f:
+            while True:
+                data = f.read(BUF_SIZE)
+                if not data:
+                    break
+                sha1.update(data)
+        return sha1.hexdigest()
+
+    async def search_file(self, file_name: str, file_path: str) -> list:
+        raw_url = "https://graph.microsoft.com/v1.0/me/drive/root/search(q='" + file_name + "')?select=name,id,size,file"
+        item_list = await self.client.me.drive.with_url(raw_url).get()
+        # item_list.additional_data["value"][0]["name"]
+        matched_items = []
+        for item in item_list.additional_data["value"]:
+            if item["size"] == os.path.getsize(file_path):
+                if item["file"]["hashes"]["sha256Hash"] is not "":
+                    if self.sha256sum(file_path).lower() == item["file"]["hashes"]["sha256Hash"].lower():
+                        matched_items.append(item)
+                elif item["file"]["hashes"]["sha1Hash"] is not "":
+                    if self.sha1sum(file_path).lower() == item["file"]["hashes"]["sha1Hash"].lower():
+                        matched_items.append(item)
+        return matched_items
---
+++ main.py
@@ -0,0 +1,45 @@
+import asyncio
+import tkinter as tk
+from tkinter import ttk, messagebox, filedialog
+import os
+import graph_api
+import tkinter as tk
+from tkinter import messagebox
+import glob
+from pathlib import Path
+
+
+async def main(local_drive_path):
+    if not local_drive_path:
+        messagebox.showerror("Error", "Please enter local drive path.")
+        exit()
+
+    if not os.path.exists(local_drive_path):
+        messagebox.showerror("Error", "Local drive path does not exist.")
+        exit()
+    graph_api_helper = graph_api.GraphAPI()
+
+    # Get list of files in the local drive
+    image_extensions = ['*.jpg', '*.jpeg', '*.png', '*.mp4', '*.mpg', "*.mov", "*.mp4", "*.MTS", "*.avi", "*.heif",
+                        "*.heifs", "*.heic", "*.heics", "*.avci", "*.avcs", "*.hif"]
+
+    for ext in image_extensions:
+        pathlist = Path(local_drive_path).rglob("**/" + ext)
+        for path in pathlist:
+            path_in_str = str(path)
+            matched_files = await graph_api_helper.search_file(str(path.name), path_in_str)
+            if len(matched_files) is not 0:
+                os.remove(path_in_str)
+                print("Deleting " + path_in_str)
+
+        pathlist = Path(local_drive_path).rglob("**/" + ext.upper())
+        for path in pathlist:
+            path_in_str = str(path)
+            matched_files = await graph_api_helper.search_file(str(path.name), path_in_str)
+            if len(matched_files) is not 0:
+                os.remove(path_in_str)
+                print("Deleting " + path_in_str)
+
+
+local_drive_path = input("Enter Drive location: ")
+asyncio.run(main(local_drive_path))
